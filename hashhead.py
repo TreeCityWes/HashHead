8c228b90065827500c4afb8b98d73f397774cfce
Wes Powell <wes@treecitytrading.us>
2023-09-29 16:54:40 -0400

    Update hashhead.py

--- hashhead.py
+++ hashhead.py
@@ -4,37 +4,19 @@ import json
 from datetime import datetime
 import os
 
+
 # Define the URLs
 url1 = "http://xenminer.mooo.com:4448/leaderboard"
 url2 = "http://xenminer.mooo.com/leaderboard"
 total_blocks_url = "http://xenminer.mooo.com/total_blocks"  
 url_xuni = "http://xenminer.mooo.com/get_xuni_counts"
 
+
 # Send HTTP request and parse the HTML content of the page with BeautifulSoup
 response1 = requests.get(url1)
 response2 = requests.get(url2)
 soup1 = BeautifulSoup(response1.text, 'html.parser')
 soup2 = BeautifulSoup(response2.text, 'html.parser')
-
-def get_super_blocks(account):
-    url = f"http://xenminer.mooo.com/get_super_blocks/{account}"
-    try:
-        response = requests.get(url, timeout=20)  # Increase timeout to 20 seconds
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('super_blocks', 0)
-        else:
-            print(f"Error: Received status code {response.status_code} from {url}")
-            return 0
-    except ConnectTimeout:
-        print(f"Error: Connection to {url} timed out.")
-        return 0
-    except RequestException as e:
-        print(f"Error: Unable to get data from {url}, due to {str(e)}")
-        return 0
-    except json.JSONDecodeError:
-        print(f"Error decoding JSON from {url}")
-        return 0
 
 # Initialize a list to store the account data
 account_data = []
@@ -49,36 +31,28 @@ if response_xuni.status_code == 200:
     except json.JSONDecodeError:
         print("Error decoding JSON from /get_xuni_counts endpoint")
 
-# Extract and Process Account Data from the second URL
-for row in soup2.select('table tr')[1:]:
+# Extract and Process Account Data from the first URL
+for row in soup1.select('table tr')[1:]:
     cols = row.select('td')
     if not cols:
         continue
     rank = int(cols[0].text.strip())
     account = cols[1].text.strip()
     total_blocks = int(cols[2].text.strip())
-    super_blocks = get_super_blocks(account)
-    total_hashes_per_second = cols[4].text.strip()
-
-    for entry in account_data:
-        if entry['account'] == account:
-            entry['total_hashes_per_second'] = total_hashes_per_second
-            break
-    else:
-        if rank <= 25000:
-            account_data.append({
-                'rank': rank,
-                'account': account,
-                'total_blocks': total_blocks,
-                'super_blocks': super_blocks,
-                'total_hashes_per_second': total_hashes_per_second,
-                'daily_blocks': 'Sub-500 Rank'
-            })
-
+    super_blocks = int(cols[3].text.strip())
+    daily_blocks = cols[4].text.strip()
+    account_data.append({
+        'rank': rank,
+        'account': account,
+        'total_blocks': total_blocks,
+        'super_blocks': super_blocks,
+        'daily_blocks': daily_blocks
+    })
 # After fetching the account_data, append the Xuni Counts
 for entry in account_data:
     account = entry.get('account', '')
     entry['total_xuni'] = xuni_counts.get(account, '0')
+
 
 # Extract Network Stats
 network_stats = {}
@@ -114,8 +88,32 @@ for soup in [soup1, soup2]:
 # Write the network stats to a separate JSON file
 with open('network_stats.json', 'w') as f:
     json.dump(network_stats, f, indent=4)
+# Extract and Process Account Data from the second URL
+for row in soup2.select('table tr')[1:]:
+    cols = row.select('td')
+    if not cols:
+        continue
+    rank = int(cols[0].text.strip())
+    account = cols[1].text.strip()
+    total_blocks = int(cols[2].text.strip())
+    super_blocks = int(cols[3].text.strip())
+    total_hashes_per_second = cols[4].text.strip()
 
-# Final cleanup of account_data and write it to JSON
+    for entry in account_data:
+        if entry['account'] == account:
+            entry['total_hashes_per_second'] = total_hashes_per_second
+            break
+    else:
+        if rank <= 25000:
+            account_data.append({
+                'rank': rank,
+                'account': account,
+                'total_blocks': total_blocks,
+                'super_blocks': super_blocks,
+                'total_hashes_per_second': total_hashes_per_second,
+                'daily_blocks': 'Sub-500 Rank'
+            })
+
 account_data = account_data[:25000] + [{'account': entry['account'], 'status': 'Out of top 25000'} for entry in account_data[25000:]]
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 account_output_data = {'timestamp': timestamp, 'data': account_data}
